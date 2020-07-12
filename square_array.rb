f01bef8b5342262a6b99bc10c81def00eb3baeeb
literal-bookmark-0123 <ben.stephanus@gmail.com>
2020-07-12 19:27:57 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,12 +1,9 @@
 def square_array(array)
-#   new_array = []
-# array.each do |integer|
-#   new_array << integer**2
-#   end
-#   new_array
-new_array = array.collect do |integer| 
-  integer**2
-end
-new_array
+  new_array = []
+array.each do |integer|
+  new_array << integer**2
+  end
+  new_array
+
 end
 
